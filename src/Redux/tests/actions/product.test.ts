8ce482f10b72b7cf1b476bfc4685ac984a40034b
Usman Ali <uali@modern-networks.co.uk>
2020-06-01 11:29:00 +0100

    added tests for action creators

--- src/Redux/tests/actions/product.test.ts
+++ src/Redux/tests/actions/product.test.ts
@@ -1,46 +1,38 @@
-//import the action to mock
-import { getAllProducts, ProductActionTypes } from '../../actions/products';
-
-// import configreStore to create a mock store where we will dispatch our actions
-import configureStore from 'redux-mock-store';
-//import thunk middle to make our action asyncronous
+import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-
-// important:  this i have used to mock the axios call
-import MockAdapter from 'axios-mock-adapter';
-// import axios dependency
-import axios from 'axios';
+import moxios from 'moxios';
+import { getAllProducts, ProductActionTypes } from '../../actions/products';
 import { initialStateWithData } from '../reducers/product.data';
 
-// declare middlewares
 const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-// initialize mockStore which is only the configureStore method which take middlesware as its parameters
-const mockStore = configureStore(middlewares);
-
-//creating a mock instance from the MockAdapter of axios
-const mock = new MockAdapter(axios);
-
-const store = mockStore({});
-
-// firing up the test Suite
-describe('Testing getAllProducts()', () => {
-  beforeEach(() => {
-    // Runs before each test in the suite
-    store.clearActions();
+describe('getAllProducts actions', () => {
+  beforeEach(function () {
+    moxios.install();
   });
-  it('should get GET_PRODUCTS', () => {
-    mock.onGet('/products').reply(200, {
-      products: initialStateWithData,
+
+  afterEach(function () {
+    moxios.uninstall();
+  });
+
+  it('creates GET_PRODUCTS Actions once succesfully fetched the post', () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: initialStateWithData,
+      });
     });
 
-    store.dispatch<any>(getAllProducts()).then(() => {
-      let expectedActions = [
-        {
-          type: ProductActionTypes.GET_PRODUCTS,
-          products: initialStateWithData,
-        },
-      ];
+    const expectedActions = [
+      { type: ProductActionTypes.GET_PRODUCTS, products: initialStateWithData },
+    ];
+
+    const store = mockStore({ posts: {} });
+
+    return store.dispatch<any>(getAllProducts()).then(() => {
+      // return of async actions
       expect(store.getActions()).toEqual(expectedActions);
     });
   });
